f459052aa1f8ceac96fa8ea7d95182a1dd65515e
FizaYasmeen <fizayasmeen99@gmail.com>
2025-08-01 13:51:55 +0530

    updated file

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,8 +6,12 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    password:{
+        type: String,
+        required: true,
+    },
     role: {
-        tyoe: String,
+        type: String,
         required: true,
         enum: ["admin", "manager", "user"],
     },
